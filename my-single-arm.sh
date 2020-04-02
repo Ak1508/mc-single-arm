e54cf944af7316991d1410bb3974cc9e9d011a83
Abishek Karki <ak1508@msstate.edu>
2020-04-02 07:59:36 -0400

    infile name changed to accomodate other angles too

---
+++ my-single-arm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+targName=$1
+
+echo ${pwd}
+
+# cd /src
+
+# make clean
+# make
+
+# echo pwd
+
+# cd ../
+
+# ./run_mc_single_arm ${targName}
+
+
+#source  /apps/root/6.10.02/setroot_CUE.bash
+
+#eval h2root worksim/${targName}.rzdat
